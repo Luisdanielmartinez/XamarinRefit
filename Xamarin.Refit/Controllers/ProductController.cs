fb8838fc9de098ff25c3cea62f0cdeb92849bccb
Luis Daniel Martinez <luisdanielcurso@hotmail.com>
2019-07-25 22:35:57 -0400

    Add the method of add image in the backend

--- Xamarin.Refit/Controllers/ProductController.cs
+++ Xamarin.Refit/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -36,16 +37,32 @@ namespace Xamarin.Refit.Controllers
             return Ok(Product);
         }
         [HttpPost]
-        public ActionResult POST([FromBody] Product product)
+        public async Task <ActionResult> POST([FromBody] ImagenViewModel product)
         {
             if (ModelState.IsValid)
             {
                 _context.Products.Add(product);
                 _context.SaveChanges();
+                var path = string.Empty;
+
+                if (product.ImageFile != null && product.ImageFile.Length > 0)
+                {
+                    path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\Products", product.ImageFile.FileName);
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await product.ImageFile.CopyToAsync(stream);
+                    }
+
+                    path = $"~/images/Products/{product.ImageFile.FileName}";
+                }
+
+
                 return new CreatedAtRouteResult("creado", new { id = product.Id });
             }
             return BadRequest(ModelState);
 
         }
+
     }
 }
---
+++ Xamarin.Refit/Models/ImagenViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xamarin.Refit.Models
+{
+    public class ImagenViewModel:Product
+    {
+        //esto hace que podamos suvirlo al servidor
+        [Display(Name="Image")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
